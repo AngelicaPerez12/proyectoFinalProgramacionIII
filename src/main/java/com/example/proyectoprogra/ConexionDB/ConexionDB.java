f3de964289cd4b3e7cede91ac1a7c440131aac5f
AngelicaPerez12 <ap22010>
2025-11-15 17:57:59 -0600

    agregando conexion

--- src/main/java/com/example/proyectoprogra/ConexionDB/ConexionDB.java
+++ src/main/java/com/example/proyectoprogra/ConexionDB/ConexionDB.java
@@ -1,4 +1,25 @@
 package com.example.proyectoprogra.ConexionDB;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class ConexionDB {
+    private static final String url= "jdbc:postgresql://localhost:5432/db_proyectoProgra";
+
+    private static final String USER = "postgres";
+
+    private static final String PASS = "admin123";
+
+    public static Connection getConection(){
+        try {
+            Connection conectar = DriverManager.getConnection(url, USER, PASS);
+            System.out.println("Conectado a la base");
+            return conectar;
+        } catch (SQLException e) {
+            throw new RuntimeException(e){
+
+            };
+        }
+    }
 }
