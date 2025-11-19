2f6366f129927b8d838f7ba68aa5f51784a7442e
AngelicaPerez12 <ap22010>
2025-11-18 21:31:00 -0600

    primer vista de cliente

--- src/main/java/com/example/proyectoprogra/controllers/DashboardClienteController.java
+++ src/main/java/com/example/proyectoprogra/controllers/DashboardClienteController.java
@@ -1,4 +1,26 @@
 package com.example.proyectoprogra.controllers;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
 public class DashboardClienteController {
+    @FXML
+    void hacerpedido(ActionEvent event) {
+
+    }
+
+    @FXML
+    void historial(ActionEvent event) {
+
+    }
+
+    @FXML
+    void perfil(ActionEvent event) {
+
+    }
+
+    @FXML
+    void vercatalogo(ActionEvent event) {
+
+    }
 }
